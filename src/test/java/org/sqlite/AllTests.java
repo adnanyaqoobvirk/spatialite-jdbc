903b12a8b95cbe4b2288bb02093ffe47be1e5078
Adnan Yaqoob Virk <adnan@yaqoob.org>
2016-05-07 19:51:37 +0500

    sqlite-jdbc code with build system changed to gradle

---
+++ src/test/java/org/sqlite/AllTests.java
@@ -0,0 +1,32 @@
+package org.sqlite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.sqlite.util.OSInfoTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    BackupTest.class,
+    ConnectionTest.class,
+    DBMetaDataTest.class,
+    ExtendedCommandTest.class,
+    ExtensionTest.class,
+    FetchSizeTest.class,
+    InsertQueryTest.class,
+    JDBCTest.class,
+    OSInfoTest.class,
+    PrepStmtTest.class,
+    QueryTest.class,
+    ReadUncommittedTest.class,
+    RSMetaDataTest.class,
+    SavepointTest.class,
+    SQLiteDataSourceTest.class,
+    SQLiteConnectionPoolDataSourceTest.class,
+    SQLiteJDBCLoaderTest.class,
+    StatementTest.class,
+    TransactionTest.class,
+    UDFTest.class
+})
+public class AllTests {
+// runs all Tests
+}
